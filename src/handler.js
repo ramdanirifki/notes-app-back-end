3db5d1e7d8fd919b7627908c7a823252d758dd0c
rifki ramdani <ramdanirifki45@gmail.com>
2023-05-27 19:21:37 +0700

    fix

--- src/handler.js
+++ src/handler.js
@@ -18,7 +18,7 @@ const addNoteHandler = (request, h) => {
 
   if (isSuccess) {
     const response = h.response({
-      status: 'successcel',
+      status: 'success',
       message: 'Catatan berhasil ditambahkan',
       data: {
         noteId: id,
